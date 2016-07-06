ded03f8fc7dbc98ed2d655ad55835ba25501bc82
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-05 18:29:10 -0700

    Adds validation and error handling

--- routes/articles.js
+++ routes/articles.js
@@ -6,6 +6,8 @@ var Article = require('../models').Article;
 router.get('/', function(req, res, next) {
   Article.findAll({order: [["createdAt", "DESC"]]}).then(function (articles) {
     res.render("articles/index", {articles: articles, title: "My Awesome Blog" });
+  }).catch(function (err) {
+    res.sendStatus(500);
   });
 });
 
@@ -13,6 +15,20 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   Article.create(req.body).then(function (article) {
     res.redirect("/articles/" + article.id);
+  }).catch(function (err) {
+    if (err.name === "SequelizeValidationError") {
+      //render
+      res.render("articles/new", {
+        article: Article.build(req.body),
+        title: "New Article",
+        errors: err.errors
+      });
+    } else {
+      // throw error to be handled by final catch
+      throw err;
+    }
+  }).catch(function (err) {
+    res.sendStatus(500);
   });
 });
 
@@ -24,41 +40,84 @@ router.get('/new', function(req, res, next) {
 /* Edit article form. */
 router.get("/:id/edit", function(req, res, next){
   Article.findById(req.params.id).then(function (article) {
-    res.render("articles/edit", {article: article, title: "Edit Article"});
+    if (article) {
+      res.render("articles/edit", {article: article, title: "Edit Article"});
+    } else {
+      res.sendStatus(404);
+    }
+  }).catch(function (err) {
+    res.sendStatus(500);
   });
 });
-
 
 /* Delete article form. */
 router.get("/:id/delete", function(req, res, next){
   Article.findById(req.params.id).then(function (article) {
-    res.render("articles/delete", {article: article, title: "Delete Article"});
+    if (article) {
+      res.render("articles/delete", {article: article, title: "Delete Article"});
+    } else {
+      res.sendStatus(404);
+    }
+  }).catch(function (err) {
+    res.sendStatus(500);
   });
 });
-
 
 /* GET individual article. */
 router.get("/:id", function(req, res, next){
   Article.findById(req.params.id).then(function (article) {
-    res.render("articles/show", {article: article, title: article.title});
+    if (article) {
+      res.render("articles/show", {article: article, title: article.title});
+    } else {
+      res.sendStatus(404);
+    }
+  }).catch(function (err) {
+    res.sendStatus(500);
   });
 });
 
 /* PUT update article. */
 router.put("/:id", function(req, res, next){
   Article.findById(req.params.id).then(function (article) {
-    return article.update(req.body);
+    if (article) {
+      return article.update(req.body);
+    } else {
+      res.sendStatus(404)
+    }
   }).then(function (article) {
     res.redirect("/articles/" + article.id);
+  }).catch(function (err) {
+    if (err.name === "SequelizeValidationError") {
+      var article = Article.build(req.body);
+      article.id = req.params.id;
+
+      //render
+      res.render("articles/edit", {
+        article: article,
+        title: "Edit Article",
+        errors: err.errors
+      });
+    } else {
+      // throw error to be handled by final catch
+      throw err;
+    }
+  }).catch(function (err) {
+    res.sendStatus(500);
   });
 });
 
 /* DELETE individual article. */
 router.delete("/:id", function(req, res, next){
   Article.findById(req.params.id).then(function (article) {
-    return article.destroy();
+    if (article) {
+      return article.destroy();
+    } else {
+      res.sendStatus(404)
+    }
   }).then(function () {
     res.redirect("/articles");
+  }).catch(function (err) {
+    res.sendStatus(500);
   });
 });
 
