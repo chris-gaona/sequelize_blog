abf39be299374bb27ba24d4dda264c797563e27e
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-06 19:25:10 -0700

    Adds functionality to delete a comment

--- routes/comments.js
+++ routes/comments.js
@@ -6,22 +6,44 @@ var express = require('express');
 var router = express.Router();
 var Comment = require('../models').Comment;
 
-/* GET get comments. */
-router.get('/', function(req, res, next) {
-  Comment.findAll({}).then(function (comments) {
-    console.log(comments);
-    res.json(comments);
-  });
-});
-
-// add new todo
+// add new comment
 router.post('/articles/:id/comment', function(req, res) {
   Comment.create({
     comment: req.body.comment,
     author: req.body.author,
     ArticleId: req.params.id
-  }).then(function(comments) {
+  }).then(function() {
     res.redirect("/articles/" + req.params.id);
+  });
+});
+
+/* Delete comment form. */
+router.get("/articles/:id/comments/:commentId/delete", function(req, res, next){
+  var article = {};
+  article.id = req.params.id;
+  Comment.findById(req.params.commentId).then(function (comment) {
+    if (comment) {
+      res.render("articles/delete_comment", {article: article, comment: comment, title: "Delete Comment"});
+    } else {
+      res.sendStatus(404);
+    }
+  }).catch(function (err) {
+    res.sendStatus(500);
+  });
+});
+
+/* DELETE individual comment. */
+router.delete("/articles/:id/comment/:commentId", function(req, res, next){
+  Comment.findById(req.params.commentId).then(function (comment) {
+    if (comment) {
+      return comment.destroy();
+    } else {
+      res.sendStatus(404)
+    }
+  }).then(function () {
+    res.redirect("/articles/" + req.params.id);
+  }).catch(function (err) {
+    res.sendStatus(500);
   });
 });
 
